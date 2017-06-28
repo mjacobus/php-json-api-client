e6a507bc44fc05479aa58bb1b1eade8521f39790
pinkgothic <pinkgothic@gmail.com>
2017-06-28 10:38:37 +0200

    Add ability to supply headers per verb (#3)

--- src/JsonInterface.php
+++ src/JsonInterface.php
@@ -7,50 +7,55 @@ interface JsonInterface
     /**
      * @param string $path
      * @param array $data
+     * @param array $options additional options to send with the request, if any
      *
      * @throws Exception
      *
      * @return array
      */
-    public function get($path, array $data = []);
+    public function get($path, array $data = [], array $options = []);
 
     /**
      * @param string $path
      * @param array $data
+     * @param array $options additional options to send with the request, if any
      *
      * @throws Exception
      *
      * @return array
      */
-    public function post($path, array $data = []);
+    public function post($path, array $data = [], array $options = []);
 
     /**
      * @param string $path
      * @param array $data
+     * @param array $options additional options to send with the request, if any
      *
      * @throws Exception
      *
      * @return array
      */
-    public function put($path, array $data = []);
+    public function put($path, array $data = [], array $options = []);
 
     /**
      * @param string $path
      * @param array $data
+     * @param array $options additional options to send with the request, if any
      *
      * @throws Exception
      *
      * @return array
      */
-    public function delete($path, array $data = []);
+    public function delete($path, array $data = [], array $options = []);
 
     /**
      * @param string $path
      * @param array $data
+     * @param array $options additional options to send with the request, if any
      *
      * @throws Exception
      *
      * @return array
      */
-    public function patch($path, array $data = []);
+    public function patch($path, array $data = [], array $options = []);
 }
